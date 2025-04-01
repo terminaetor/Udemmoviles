49e6309f20dc31e9a839fc3fa1cfd3069ddbb70a
Jacob <jacc20223@gmail.com>
2025-04-01 11:27:31 -0500

    Creacion script acCriaturas

---
+++ Assets/acScripts/acCriaturas.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]//indica que una clase o estructura puede ser convertida en datos como json
+public class acCriaturas
+{
+    public int id;
+    public string nombre;
+    public bool atrapado;
+}
